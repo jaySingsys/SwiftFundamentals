8953c75138499bb7d7282330cbccd430070c73e4
Jay Prakash Sharma <jay@singsys.com>
2022-05-18 11:36:11 +0530

    Initial Commit

---
+++ errorHandling.playground/Contents.swift
@@ -0,0 +1,25 @@
+//enum cases: Error{
+//    case isValidLicence
+//    case isValidpPassport
+//}
+//
+////Error Handling
+//func throwError() throws {
+//    
+//}
+//
+//do {
+//    try cases.isValidLicence()
+//    
+//    
+//}catch{
+//    
+//}
+let quotation = """
+    The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
+print(quotation)
