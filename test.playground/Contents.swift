a1578e594dc81518f307699b6c3c5fd71c4ac3d8
Jay Prakash Sharma <jay@singsys.com>
2022-05-26 10:08:19 +0530

    New features added

---
+++ test.playground/Contents.swift
@@ -0,0 +1,11 @@
+//
+struct StudentData{
+   var name:String
+     func enterData(std:String)-> String{
+        return std
+        }
+    }
+
+StudentData.init(name: "Hello")
+
+StudentData.enterData
